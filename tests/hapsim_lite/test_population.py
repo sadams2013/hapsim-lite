f27d5bac3c1b98834d1b83ed26a88754e8d0a99e
Solomon Adams <sadams2013@gmail.com>
2025-10-26 10:16:58 -0400

    adds docstring for poplation data test

--- tests/hapsim_lite/test_population.py
+++ tests/hapsim_lite/test_population.py
@@ -3,4 +3,5 @@ import pytest
 from . import pop_data
 
 def test_init(pop_data):
+    """Population Data init without error and with expected variant index length"""
     assert len(pop_data.variant_index) == 5
